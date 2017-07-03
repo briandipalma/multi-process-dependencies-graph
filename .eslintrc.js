a9a8ba64e2fc43640f3a54ea91a02eb0899891f9
Brian Di Palma <offler@gmail.com>
2017-07-03 09:46:56 +0100

    chore: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const baseRules = require("eslint-config-airbnb-base/rules/style");
+
+const [_, ...restricted] = baseRules.rules["no-restricted-syntax"];
+
+module.exports = {
+  extends: ["airbnb-base", "prettier"],
+  rules: {
+    // The console is the easiest way to provide feedback when executing in node
+    "no-console": 0,
+    "no-debugger": 0,
+    // Let's allow for..in https://github.com/airbnb/javascript/issues/1122
+    "no-restricted-syntax": [
+      2,
+      ...restricted.filter(r => r.selector !== "ForOfStatement")
+    ],
+    "no-underscore-dangle": ["error", { allowAfterThis: true }]
+  }
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Multi process dependencies graph
